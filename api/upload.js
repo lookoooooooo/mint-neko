1862886a47d967fb5181ad6ab06e11c5f73e2147
lookoo <imanydadueva@gmail.com>
2026-08-26 20:45:00 +0800

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,16 +1,12 @@
 import { put } from '@vercel/blob';
 
 export default async function handler(request, response) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const filename = searchParams.get('filename');
+  var url = new URL(request.url, 'http://localhost');
+  var filename = url.searchParams.get('filename');
 
-    const blob = await put(filename, request, {
-      access: 'public',
-    });
+  var blob = await put(filename, request, {
+    access: 'public'
+  });
 
-    return response.status(200).json(blob);
-  } catch (error) {
-    return response.status(500).json({ error: error.message });
-  }
+  return response.status(200).json(blob);
 }
