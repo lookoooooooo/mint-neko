8ab0f29d98cc8ce21d5c10702704561de91fdac2
lookoo <imanydadueva@gmail.com>
2026-08-26 21:31:26 +0800

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,6 +1,10 @@
 import { put } from '@vercel/blob';
 
 export default async function handler(request, response) {
+  if (request.method !== 'POST') {
+    return response.status(405).json({ error: 'Method not allowed' });
+  }
+
   var url = new URL(request.url, 'http://localhost');
   var filename = url.searchParams.get('filename');
 
@@ -10,3 +14,9 @@ export default async function handler(request, response) {
 
   return response.status(200).json(blob);
 }
+
+export var config = {
+  api: {
+    bodyParser: false
+  }
+};
